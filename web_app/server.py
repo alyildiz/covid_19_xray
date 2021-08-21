3ecb28136118199b4d87615fd21bbd36a24bdc6f
ALYILDIZ Baris <50208966+alyildiz@users.noreply.github.com>
2021-08-21 19:16:07 +0200

    Update server.py

--- web_app/server.py
+++ web_app/server.py
@@ -5,8 +5,6 @@ from web_app.utils import model_inference, setup_annotation, setup_parameters
 
 model = LitModel.load_from_checkpoint(checkpoint_path=DEMO_MODEL_CKPT)
 
-reverse_encoding = {v: k for k, v in class_dic.items()}
-
 image = setup_parameters()
 out_image = image.copy()
 proba = model_inference(model, out_image)
