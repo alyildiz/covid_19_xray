ad1993747c33c6d772099db9981a141cad797e3d
Baris <alyildizb@gmail.com>
2021-08-21 12:15:19 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Xray classification using pre trained model',
+    author='ALYILDIZ Baris',
+    license='MIT',
+)
